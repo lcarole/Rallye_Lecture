b61b70eed25ea936644b2cd1a4e9d49bf73b62d9
lcarole <lcarole@sio.jjr>
2019-10-16 13:55:03 +0200

    commit initial

---
+++ ImportCSV/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace classbibli
+{
+    public class Connection
+    {
+        MySqlConnection cnx;
+        public MySqlConnection Cnx
+        { get => cnx; set => cnx = value; }
+        public Connection(string h, string user, string pwd, string bdd)
+        {
+            string sCnx = String.Format("server={0};uid={1};database={2};port=3306;pwd={3}", h, user, bdd, pwd);
+            this.cnx = new MySqlConnection(sCnx);
+        }
+        public string ouvrir()
+        {
+            string message;
+            try
+            {
+                cnx.Open();
+                message = "connexion réussie";
+            }
+            catch (Exception e)
+            {
+                message = String.Format("echec de la connexion {0}", e.Message);
+            }
+            return message;
+        }
+        public void fermer()
+        {
+            cnx.Close();
+        }
+
+    }
+}
---
+++ ImportCSV/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using classbibli;
+
+namespace rallylecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message;
+            Connection conn = new Connection("localhost", "root", "siojjr", "rallyelecture");
+            message = conn.ouvrir();
+            Console.WriteLine(message);
+            conn.fermer();
+            Console.ReadLine();
+
+            conn.ouvrir();
+
+            MySqlCommand cmd =new MySqlCommand();
+
+            cmd.Connection = conn.Cnx; 
+            cmd.CommandText = "select * from auteur";
+            cmd.CommandType = System.Data.CommandType.Text;
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr["id"].ToString()+" "+ rdr["nom"].ToString()); 
+            }
+            conn.fermer();
+            Console.ReadLine();
+            
+
+
+
+
+        }
+    }
+}

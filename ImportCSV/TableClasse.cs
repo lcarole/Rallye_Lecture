85f019233a772e9a8bf0ed634bc5aa1cf21bcb69
lcarole <52493764+lcarole@users.noreply.github.com>
2019-11-21 16:02:26 +0100

    Update TableClasse.cs

--- ImportCSV/TableClasse.cs
+++ ImportCSV/TableClasse.cs
@@ -1,16 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace rallyeLecture
 {
     class TableClasse
     {
-        public Classe Insert(Classe classe)
+        public void Insert(Classe classe)
         {
+            Connection Cnx = new Connection("localhost", "root", "siojjr", "rallyelecture");
+            string idEnseignant = Convert.ToString(classe.GetIDEnseignant);
+            string anneeScolaire = classe.GetanneeScolaire;
+            string idNiveau = Convert.ToString(classe.GetIDniveau);
+            string rq = string.Format("insert into classe(idEnseignant, anneeScolaire, id Niveau) values("+idEnseignant+","+anneeScolaire+","+idNiveau+")");
 
+            Cnx.ouvrir();
+            MySqlCommand Cmd = new MySqlCommand(rq, Cnx.Cnx);
+            MySqlDataReader Rdr = Cmd.ExecuteReader();
+            Cnx.fermer();
         }
     }
 }

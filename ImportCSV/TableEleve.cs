c4036aaad7ee39c32eea029b21080d4ace6d0c6d
val95 <52490065+val95@users.noreply.github.com>
2019-11-27 14:46:27 +0100

    Update TableEleve.cs

--- ImportCSV/TableEleve.cs
+++ ImportCSV/TableEleve.cs
@@ -8,5 +8,19 @@ namespace RallyeLecture
 {
     class TableEleve
     {
+         public void InsertEleve(Eleve eleve)
+        {
+         Connection Cnx = new Connection("172.16.0.149", "root", "siojjr", "rallyelecture");
+            string nom = eleve.Nom;
+            string prenom = eleve.Prenom;
+            string login = eleve.GetLogin;
+            string rq = string.Format("insert into classe(nom,prenom,login) values(" + nom + "," + prenom + "," + login + ")");
+
+
+            Cnx.ouvrir();
+            MySqlCommand Cmd = new MySqlCommand(rq, Cnx.Cnx);
+            MySqlDataReader Rdr = Cmd.ExecuteReader();
+            Cnx.fermer();
+         }
     }
 }
